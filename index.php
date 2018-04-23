573cc6fdb0f01e7a58d2923850ffc9073bd4a6c4
Basitomania <basitabdulkareeem@gmail.com>
2018-04-23 23:21:20 +0100

    commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        <?php
+        echo "Today is " . date("Y.m.d") .
+        "<br>";
+        echo "Today is " . date("1");
+        ?>
+    </body>
+</html>
